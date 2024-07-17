006b6a1d98c0d01be0ba739374edb84725e072bc
DanielMxOrellana <daorellana.2@sudamericano.edu.ec>
2024-07-17 09:02:36 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pantalla01Component } from './pages/pantalla-01/pantalla-01.component';
+import { Pantalla02Component } from './pages/pantalla-02/pantalla-02.component';
+import { Pantalla03Component } from './pages/pantalla-03/pantalla-03.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Pantalla01Component, Pantalla02Component, Pantalla03Component]
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
