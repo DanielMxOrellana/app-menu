4c5856b00295497fd8ac4a336c1b8272093175b4
DanielMxOrellana <daorellana.2@sudamericano.edu.ec>
2024-07-17 08:43:40 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
+import { Pantalla01Component } from './pages/pantalla-01/pantalla-01.component';
+import { Pantalla02Component } from './pages/pantalla-02/pantalla-02.component';
 
 export const routes: Routes = [
     {
-        path : 'pantalla-01Component'
-    }
+        path : 'pantalla-01Component',
+        component: Pantalla01Component
+    },
+    {
+        path: 'pantalla-02Component',
+        component: Pantalla02Component
+    },
+    {
+        path: 'pantalla-03Component',
+    },
+
 ];
