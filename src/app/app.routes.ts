ec7c4057c81465d63593a808a4f288434f0186af
Paul Muela <pjmuela@sudamericano.edu.ec>
2024-07-17 08:54:51 -0500

    commit-02

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Pantalla01Component } from './pages/pantalla-01/pantalla-01.component';
 import { Pantalla02Component } from './pages/pantalla-02/pantalla-02.component';
+import { Pantalla03Component } from './pages/pantalla-03/pantalla-03.component';
 
 export const routes: Routes = [
     {
@@ -13,6 +14,6 @@ export const routes: Routes = [
     },
     {
         path: 'pantalla-03Component',
+        component: Pantalla03Component
     },
-
 ];
