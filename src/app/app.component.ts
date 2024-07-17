757d50926f40df9a79155e0582cc2778cc5c2e38
DanielMxOrellana <daorellana.2@sudamericano.edu.ec>
2024-07-17 17:27:41 -0500

    03 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,12 @@ import { Pantalla01Component } from './pages/pantalla-01/pantalla-01.component';
 import { Pantalla02Component } from './pages/pantalla-02/pantalla-02.component';
 import { Pantalla03Component } from './pages/pantalla-03/pantalla-03.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Pantalla01Component, Pantalla02Component, Pantalla03Component]
+  imports: [RouterOutlet, Pantalla01Component, Pantalla02Component, Pantalla03Component],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app-menu';
