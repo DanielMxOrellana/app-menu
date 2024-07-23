1e41eb07e463511fda61826fcb5c3db0bb70f7b6
DanielMxOrellana <daorellana.2@sudamericano.edu.ec>
2024-07-23 11:42:15 -0500

    03 commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,10 @@ import { Pantalla01Component } from './pages/pantalla-01/pantalla-01.component';
 import { Pantalla02Component } from './pages/pantalla-02/pantalla-02.component';
 import { Pantalla03Component } from './pages/pantalla-03/pantalla-03.component';
 
+
+
 export const routes: Routes = [
-    {
-        path : 'pantalla-01Component',
-        component: Pantalla01Component
-    },
-    {
-        path: 'pantalla-02Component',
-        component: Pantalla02Component
-    },
-    {
-        path: 'pantalla-03Component',
-        component: Pantalla03Component
-    },
+    {path: 'pantalla1', component:Pantalla01Component},
+    {path: 'pantalla2', component:Pantalla02Component},
+    {path: 'pantalla3', component:Pantalla03Component},
 ];
